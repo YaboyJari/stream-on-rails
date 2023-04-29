62e4ae3b2fab30538ecb42b4530fb7c8bddb6d4e
Jari <jarivmeirvenne@gmail.com>
2023-04-29 16:44:50 +0200

    - added mouse position

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from 'react';
 import './App.scss'
 
 function App() {
+  const [mousePos, setMousePos] = useState<any>({});
+  useEffect(() => {
+    const handleMouseMove = (event: any) => {
+      setMousePos({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener(
+        'mousemove',
+        handleMouseMove
+      );
+    };
+  }, []);
   return (
-    <div>Hello World</div>
+    <div>
+      The mouse is at position{' '}
+      <b>
+        ({mousePos.x}, {mousePos.y})
+      </b>
+    </div>
   )
 }
 
